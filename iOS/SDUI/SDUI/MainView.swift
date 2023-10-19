e565fca984c7694b840e659567a5e53dcac2c032
Ferdinand <ftedjakusuma@deloitte.com>
2023-10-19 12:17:05 +0700

    WIP

---
+++ iOS/SDUI/SDUI/MainView.swift
@@ -0,0 +1,17 @@
+//
+//  HostingViewController.swift
+//  SDUI
+//
+//  Created by Tedjakusuma, Ferdinand on 05/06/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Text("Main View").font(.system(size: 36))
+            Text("Loaded by MainView").font(.system(size: 14))
+        }
+    }
+}
